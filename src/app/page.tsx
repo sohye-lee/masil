10ed2637d8bde41858e3ddd45f5e9ebe84b7b45e
Sohye <sohye@GimgasiMacSohye.fios-router.home>
2023-12-01 18:13:52 -0500

    up to date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link'
 export default function Home() {
   return (
   <div>
-    <Link href=""></Link>
+    <div className="flex items-center justify-center space-x-3">
+
+    <Link href="">Home</Link>
+    <Link href="/talks">Talks</Link>
+    <Link href="/schools">Schools</Link>
+    <Link href="/life">Life</Link>
+    </div>
   </div>
   )
 }
