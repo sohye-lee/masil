165e8d390c5a91cbebd773142537ff3c77dbb16f
sohye-lee <sohyelee@gmail.com>
2023-12-22 08:57:50 -0500

    resources added to model

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,18 +1,16 @@
-import ShoolImage from 'public/schools.jpg';
+import ShoolImage from 'public/bg-2.jpg';
 import Hero from '@/components/hero';
 import { db } from '@/db';
 
 export default async function QuestionsPage() {
-
-
-    return <div className='w-full'>
-        <Hero 
-            title="Questions" 
-            description='Ask and Get Answers' 
-            imgAlt='' 
-            imgData={ShoolImage} 
-        />
- 
+  return (
+    <div className="w-full">
+      <Hero
+        title="Questions"
+        description="Ask and Get Answers"
+        imgAlt=""
+        imgData={ShoolImage}
+      />
     </div>
- 
+  );
 }
--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,14 +1,15 @@
 import Hero from '@/components/hero';
-import TalkImage from 'public/talks.jpg';
+import TalkImage from 'public/bg-3.jpg';
 
 export default function ResourcesPage() {
-  
-    return <div className='w-full'>
-    <Hero 
-        title="Resources" 
-        description='Information from various resources' 
-        imgAlt='' 
-        imgData={TalkImage} 
-    />
-</div>
+  return (
+    <div className="w-full">
+      <Hero
+        title="Resources"
+        description="Information from various resources"
+        imgAlt=""
+        imgData={TalkImage}
+      />
+    </div>
+  );
 }
